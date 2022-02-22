be88a08e3c80e9d877e4f61ea19b18e69d11e162
Adrian Sawatzky <adrisawa188@ed.amdsb.ca>
2022-02-22 10:01:17 -0500

    Completed work in Form1 and MenuScreen and began work on GameScreen. Attempting to make buttons light up in sequence based on info from storePattern list.

--- SimonSays/Form1.cs
+++ SimonSays/Form1.cs
@@ -14,7 +14,8 @@ namespace SimonSays
 {
     public partial class Form1 : Form
     {
-        //TODO: create a List to store the pattern. Must be accessable on other screens
+        //Create a List to store the pattern. Must be accessable on other screens 
+        public static List<int> storePattern = new List<int>();
 
         public Form1()
         {
@@ -23,7 +24,37 @@ namespace SimonSays
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            //TODO: Launch MenuScreen
+            //Launch MenuScreen
+            Form1.ChangeScreen(this, new MenuScreen());
+
+        }
+            
+        public static void ChangeScreen(object sender, UserControl next)
+        {
+            Form f; // will either be the sender or parent of sender 
+
+            if (sender is Form)
+            {
+                f = (Form)sender;                          //f is sender 
+            }
+
+            else
+            {
+                UserControl current = (UserControl)sender;  //create UserControl from sender 
+
+                f = current.FindForm();                     //find Form UserControl is on 
+
+                f.Controls.Remove(current);                 //remove current UserControl 
+            }
+
+            // add the new UserControl to the middle of the screen and focus on it 
+            next.Location = new Point((f.ClientSize.Width - next.Width) / 2,
+
+                (f.ClientSize.Height - next.Height) / 2);
+
+            f.Controls.Add(next);
+
+            next.Focus();
         }
     }
 }
--- SimonSays/GameScreen.cs
+++ SimonSays/GameScreen.cs
@@ -15,6 +15,7 @@ namespace SimonSays
     public partial class GameScreen : UserControl
     {
         //TODO: create guess variable to track what part of the pattern the user is at
+        int userGuess = 0; 
 
 
         public GameScreen()
@@ -24,14 +25,61 @@ namespace SimonSays
 
         private void GameScreen_Load(object sender, EventArgs e)
         {
-            //TODO: clear the pattern list from form1, refresh, pause for a bit, and run ComputerTurn()
+            //Clear the pattern list from form1, refresh, pause for a bit, and run ComputerTurn()
+            Form1.storePattern.Clear();
+            Refresh(); 
+            Thread.Sleep(1500); 
+            ComputerTurn(); 
         }
 
         private void ComputerTurn()
         {
-            //TODO: get rand num between 0 and 4 (0, 1, 2, 3) and add to pattern list
+            //Get rand num between 0 and 4 (0, 1, 2, 3) and add to pattern list
+            Random randGen = new Random();
+            int randValue = 0;
+            randValue = randGen.Next(0, 4);
+
+            Form1.storePattern.Add(randValue);
 
             //TODO: create a for loop that shows each value in the pattern by lighting up approriate button
+            for (int i = 0; i < Form1.storePattern.Count(); i++)
+            {
+                if (Form1.storePattern.Contains(0) )
+                {
+                    greenButton.BackColor = Color.LightGreen;
+                    Refresh();
+                    Thread.Sleep(500);
+                    greenButton.BackColor = Color.ForestGreen;
+                    Refresh();
+                }
+
+                 else if (Form1.storePattern.Contains(1) )
+                {
+                    redButton.BackColor = Color.OrangeRed;
+                    Refresh();
+                    Thread.Sleep(500);
+                    redButton.BackColor = Color.DarkRed;
+                    Refresh();
+                }
+
+                else if (Form1.storePattern.Contains(2) )
+                {
+                    yellowButton.BackColor = Color.Gold;
+                    Refresh();
+                    Thread.Sleep(500);
+                    yellowButton.BackColor = Color.Goldenrod;
+                    Refresh();
+                }
+
+                else if (Form1.storePattern.Contains(3) )
+                {
+                    blueButton.BackColor = Color.LightBlue;
+                    Refresh();
+                    Thread.Sleep(500);
+                    blueButton.BackColor = Color.DarkBlue;
+                    Refresh();
+                }
+            }
 
             //TODO: get guess index value back to 0
         }
@@ -39,9 +87,9 @@ namespace SimonSays
         public void GameOver()
         {
             //TODO: Play a game over sound
-
-            //TODO: close this screen and open the GameOverScreen
-
+            
+            //Close this screen and open the GameOverScreen
+            Form1.ChangeScreen(this, new GameOverScreen());
         }
 
         //TODO: create one of these event methods for each button
